a4b8e2206abee66483c61a2fb168f01fa815261f
koralowiec <arek0906@protonmail.com>
2021-04-17 19:25:07 +0200

    Some setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Mariusz Pudzianowski");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import {Client} from "@typeit/discord";
+
+async function start() {
+    const client = new Client({
+        classes: [
+            `${__dirname}/*Discord.ts`, // glob string to load the classes
+            `${__dirname}/*Discord.js` // If you compile using "tsc" the file extension change to .js
+        ],
+        silent: false,
+        variablesChar: ":"
+    });
+
+    const token = process.env.TOKEN;
+
+    await client.login(token);
+}
+
+start();
